479b63d5974af9753ef725f0bbef7eaa08bf58e5
Alex Novak <alexwnovak@gmail.com>
2016-10-24 19:14:02 -0400

    +Plane struct

---
+++ VmfCat/VmfCat/Plane.cs
@@ -0,0 +1,27 @@
+﻿namespace VmfCat
+{
+   public struct Plane
+   {
+      public Vector3 P1
+      {
+         get;
+      }
+
+      public Vector3 P2
+      {
+         get;
+      }
+
+      public Vector3 P3
+      {
+         get;
+      }
+
+      public Plane( Vector3 p1, Vector3 p2, Vector3 p3 )
+      {
+         P1 = p1;
+         P2 = p2;
+         P3 = p3;
+      }
+   }
+}
