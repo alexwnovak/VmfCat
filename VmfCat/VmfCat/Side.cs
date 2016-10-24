11d21207df16914739fa99b67abadb6cc3a214ec
Alex Novak <alexwnovak@gmail.com>
2016-10-24 19:30:27 -0400

    +Constructor to set the plane; using some dummy values for now

--- VmfCat/VmfCat/Side.cs
+++ VmfCat/VmfCat/Side.cs
@@ -21,5 +21,13 @@
       {
          get;
       }
+
+      public Side( Plane plane )
+      {
+         Id = 0;
+         Plane = plane;
+         Material = "customdev/dev_measuregeneric01red";
+         LightMapScale = 16;
+      }
    }
 }
