0d621d3185024415f8673e06dea2813a00c1f945
Alex Novak <alexwnovak@gmail.com>
2016-10-24 19:13:29 -0400

    +Vector3 for vectors and vertices

---
+++ VmfCat/VmfCat/Vector3.cs
@@ -0,0 +1,27 @@
+﻿namespace VmfCat
+{
+   public struct Vector3
+   {
+      public int X
+      {
+         get;
+      }
+
+      public int Y
+      {
+         get;
+      }
+
+      public int Z
+      {
+         get;
+      }
+
+      public Vector3( int x, int y, int z )
+      {
+         X = x;
+         Y = y;
+         Z = z;
+      }
+   }
+}
