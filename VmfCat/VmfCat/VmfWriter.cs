57024021d7cbf4937236810717d597b78b381b2a
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:29:03 -0400

    Fleshing out VmfWriter a tad to begin serialization

--- VmfCat/VmfCat/VmfWriter.cs
+++ VmfCat/VmfCat/VmfWriter.cs
@@ -1,6 +1,22 @@
-﻿namespace VmfCat
+﻿using System.IO;
+
+namespace VmfCat
 {
    public class VmfWriter
    {
+      private readonly IWriter _writer;
+
+      public VmfWriter( IWriter writer )
+      {
+         _writer = writer;
+      }
+
+      public void Save( World world, string fileName )
+      {
+         foreach ( var solid in world.Solids )
+         {
+            solid.Serialize( _writer );
+         }
+      }
    }
 }
