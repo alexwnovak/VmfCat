7959cd3c2472365075e8e530b138f3254e4cdcbc
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:20:08 -0400

    +IWriter interface
    This will be the thing that objects use to serialize their stuff out to
    file, but technically this abstraction means they don't know (or care)
    where it's going.
    
    This interface also grants formatting (indent in and out) functionality,
    which something like a TextWriter or Stream wouldn't do. We could derive
    a new stream type, but then we wouldn't have interface mockability.

---
+++ VmfCat/VmfCat/IWriter.cs
@@ -0,0 +1,12 @@
+﻿namespace VmfCat
+{
+   public interface IWriter
+   {
+      void Write( string s );
+      void WriteLine();
+      void WriteLine( string line );
+      void WritePairLine( string property, object value );
+      void Indent();
+      void Exdent();
+   }
+}
