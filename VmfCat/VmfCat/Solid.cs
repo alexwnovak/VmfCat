3ed873a7088aeffe54a4a5aad33391575bd3ac5d
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:24:19 -0400

    Giving Solid a way to ingest sides

--- VmfCat/VmfCat/Solid.cs
+++ VmfCat/VmfCat/Solid.cs
@@ -1,15 +1,24 @@
-﻿namespace VmfCat
+﻿using System.Text;
+
+namespace VmfCat
 {
    public struct Solid
    {
       public int Id
       {
          get;
+         internal set;
       }
 
       public Side[] Sides
       {
          get;
+      }
+
+      public Solid( params Side[] sides )
+      {
+         Id = 0;
+         Sides = sides;
       }
    }
 }
