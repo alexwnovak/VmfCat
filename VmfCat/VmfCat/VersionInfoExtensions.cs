56dd15fd30ebfda9f5420f740af9f5f26d9f9626
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:32:54 -0400

    +Extension to serialize VersionInfo

---
+++ VmfCat/VmfCat/VersionInfoExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace VmfCat
+{
+   public static class VersionInfoExtensions
+   {
+      public static void Serialize( this VersionInfo vi, IWriter writer )
+      {
+         writer.WriteLine( "versioninfo" );
+         writer.WriteLine( "{" );
+
+         writer.Indent();
+
+         writer.WritePairLine( "editorversion", vi.EditorVersion );
+         writer.WritePairLine( "editorbuild", vi.EditorBuild );
+         writer.WritePairLine( "mapversion", vi.MapVersion );
+         writer.WritePairLine( "formatversion", vi.FormatVersion );
+         writer.WritePairLine( "prefab", vi.Prefab );
+
+         writer.Exdent();
+
+         writer.WriteLine( "}" );
+      }
+   }
+}
