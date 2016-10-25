c23364193bed7565be4d9b7eaaccf8b7645698c9
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:27:59 -0400

    +Solid serialization extension

---
+++ VmfCat/VmfCat/SolidExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace VmfCat
+{
+   public static class SolidExtensions
+   {
+      public static void Serialize( this Solid s, IWriter writer )
+      {
+         writer.WriteLine( "solid" );
+         writer.WriteLine( "{" );
+
+         writer.Indent();
+
+         writer.WritePairLine( "id", s.Id );
+
+         foreach ( var side in s.Sides )
+         {
+            side.Serialize( writer );
+         }
+
+         writer.Exdent();
+
+         writer.WriteLine( "}" );
+      }
+   }
+}
