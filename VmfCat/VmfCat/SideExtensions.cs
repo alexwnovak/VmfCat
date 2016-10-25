ae44d5e04708f12e6a1d4907cb6e360a525a3a64
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:27:37 -0400

    +Extensions for Side serialization

---
+++ VmfCat/VmfCat/SideExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace VmfCat
+{
+   public static class SideExtensions
+   {
+      public static void Serialize( this Side s, IWriter writer )
+      {
+         writer.WriteLine( "side" );
+         writer.WriteLine( "{" );
+
+         writer.Indent();
+         {
+            writer.WritePairLine( "id", s.Id );
+
+            writer.Write( "\"plane\" \"" );
+            s.Plane.Serialize( writer );
+            writer.Write( "\"" );
+
+            writer.WriteLine();
+
+            writer.WritePairLine( "material", s.Material );
+
+            writer.WritePairLine( "uaxis", "[1 0 0 0] 0.25" );
+            writer.WritePairLine( "vaxis", "[0 0 -1 0] 0.25" );
+
+            writer.WritePairLine( "rotation", s.Rotation );
+            writer.WritePairLine( "lightmapscale", s.LightMapScale );
+            writer.WritePairLine( "smoothing_groups", s.SmoothingGroups );
+         }
+
+         writer.Exdent();
+         writer.WriteLine( "}" );
+      }
+   }
+}
