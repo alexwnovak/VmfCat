70ad3411c58983d6465079997271db55fc26ea14
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:28:27 -0400

    +Vector3 serialization extension

---
+++ VmfCat/VmfCat/Vector3Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace VmfCat
+{
+   public static class Vector3Extensions
+   {
+      public static void Serialize( this Vector3 v, IWriter writer )
+      {
+         writer.Write( $"{v.X} {v.Y} {v.Z}" );
+      }
+   }
+}
