da416381d6a18a1cbbf3caed813748e9fc9f8560
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:17:15 -0400

    +VersionInfo class

---
+++ VmfCat/VmfCat/VersionInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace VmfCat
+{
+   public class VersionInfo
+   {
+      public int EditorVersion
+      {
+         get;
+         set;
+      } = 400;
+
+      public int EditorBuild
+      {
+         get;
+         set;
+      } = 7293;
+
+      public int MapVersion
+      {
+         get;
+         set;
+      } = 2;
+
+      public int FormatVersion
+      {
+         get;
+         set;
+      } = 100;
+
+      public int Prefab
+      {
+         get;
+         set;
+      } = 0;
+   }
+}
