2ee601a2dc76cc14fd1052789372d12dba557e7e
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:27:18 -0400

    +PlaneExtensions for serialization

---
+++ VmfCat/VmfCat/PlaneExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace VmfCat
+{
+   public static class PlaneExtensions
+   {
+      public static void Serialize( this Plane p, IWriter writer )
+      {
+         writer.Write( "(" );
+         p.P1.Serialize( writer );
+
+         writer.Write( ") (" );
+
+         p.P2.Serialize( writer );
+
+         writer.Write( ") (" );
+
+         p.P3.Serialize( writer );
+         writer.Write( ")" );
+      }
+   }
+}
