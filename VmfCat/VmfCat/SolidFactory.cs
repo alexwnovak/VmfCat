91fbaf3e06b4d319c33dc80ab833759f1b4d65b6
Alex Novak <alexwnovak@gmail.com>
2016-10-25 12:28:11 -0400

    +Factory for generating solids with IDs

---
+++ VmfCat/VmfCat/SolidFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VmfCat
+{
+   public class SolidFactory
+   {
+      private int _idCounter = 2;
+
+      public Solid Create( params Side[] sides )
+      {
+         return new Solid( sides )
+         {
+            Id = _idCounter++
+         };
+      }
+
+   }
+}
